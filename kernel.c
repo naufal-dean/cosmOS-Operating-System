40fde8d4199bc84403b83447b7ae6744ec6c7ff3
mufraswid <13518147@std.stei.itn.ac.id>
2020-02-11 20:19:55 +0700

    adding kernel.c

---
+++ kernel.c
@@ -0,0 +1,44 @@
+/* Ini deklarasi fungsi */
+void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void printString(char *string);
+void readString(char *string);
+void readSector(char *buffer, int sector);
+void writeSector(char *buffer, int sector);
+void readFile(char *buffer, char *filename, int *success);
+void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void writeFile(char *buffer, char *filename, int *sectors);
+void executeProgram(char *filename, int segment, int *success);
+
+int main() {
+  makeInterrupt21();
+  while (1);
+}
+
+void handleInterrupt21 (int AX, int BX, int CX, int DX){
+  switch (AX) {
+    case 0x0:
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    case 0x2:
+      readSector(BX, CX);
+      break;
+    case 0x3:
+      writeSector(BX, CX);
+      break;
+    case 0x4:
+      readFile(BX, CX, DX);
+      break;
+    case 0x5:
+      writeFile(BX, CX, DX);
+      break;
+    case 0x6:
+      executeProgram(BX, CX, DX);
+      break;
+    default:
+      printString("Invalid interrupt");
+  }
+}
+
