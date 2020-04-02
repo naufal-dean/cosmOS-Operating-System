ccc38d6312f412db792e466799409bea31c78ccf
naufal-dean <naufal.dean@gmail.com>
2020-04-02 20:20:51 +0700

    Add sector to save curDir and parentIdx

--- compile_viim.sh
+++ compile_viim.sh
@@ -1,8 +1,9 @@
 bcc -ansi -c -o viim/viim.o viim/viim.c
 bcc -ansi -c -o lib/appShellLinker/appShellLinker.o lib/appShellLinker/appShellLinker.c
 bcc -ansi -c -o lib/fileIO/fileIO.o lib/fileIO/fileIO.c
+bcc -ansi -c -o lib/math/math.o lib/math/math.c
 bcc -ansi -c -o lib/string/string.o lib/string/string.c
 bcc -ansi -c -o lib/text/text.o lib/text/text.c
 nasm -f as86 lib.asm -o lib.o
-ld86 -o viim/viim -d viim/viim.o lib.o lib/appShellLinker/appShellLinker.o lib/fileIO/fileIO.o lib/string/string.o lib/text/text.o
+ld86 -o viim/viim -d viim/viim.o lib.o lib/appShellLinker/appShellLinker.o lib/fileIO/fileIO.o lib/string/string.o lib/text/text.o lib/math/math.o
 ./loadFile viim/viim 0x1
--- lib/appShellLinker/appShellLinker.c
+++ lib/appShellLinker/appShellLinker.c
@@ -1,11 +1,31 @@
 #include "appShellLinker.h"
 
 void setArgs(char * args) {
-	writeSector_intr(args, 0x400);
+	writeSector_intr(args, ARGS_SECTOR);
 }
 
 void getArgs(char * args) {
-	readSector_intr(args, 0x400);
+	readSector_intr(args, ARGS_SECTOR);
+}
+
+void setCurDir(char * curDir) {
+	writeSector_intr(curDir, CUR_DIR_SECTOR);
+}
+
+void getCurDir(char * curDir) {
+	readSector_intr(curDir, CUR_DIR_SECTOR);
+}
+
+void setParIdx(int * parIdx) {
+	char temp[100];
+	intToStr(*parIdx, temp);
+	writeSector_intr(temp, PAR_IDX_SECTOR);
+}
+
+void getParIdx(int * parIdx) {
+	char temp[100];
+	readSector_intr(temp, PAR_IDX_SECTOR);
+	*parIdx = strToInt(temp);
 }
 
 void backToShell() {
